8a6e62815ee603975cafb64ae41beaa24cc03430
Mohit Badgotra <mohitbadgotra@Mohits-MacBook-Pro.local>
2025-02-04 19:31:05 +0530

    feature: handled test case for different delimeter

--- utils/stringCalculator.ts
+++ utils/stringCalculator.ts
@@ -2,6 +2,11 @@ export function add(numbers: string): number | string {
   if (!numbers) return 0;
   numbers = numbers.replace(/\\n/g, "\n");
   let delimeter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimeter = new RegExp(parts[0]?.slice(2));
+    numbers = parts[1];
+  }
   const numArray = numbers.split(delimeter).map((one) => Number(one));
   const negativeNumsArray = numArray.filter((num) => num < 0);
   if (negativeNumsArray?.length) {
