4112b513ae3976f840de1f7e3a0724dbef63f8a3
Mohit Badgotra <mohitbadgotra@Mohits-MacBook-Pro.local>
2025-02-04 19:11:34 +0530

    test and ui implemented calculator screen

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import StringCalculator from "@/app/page";
+
+describe("String Calculator UI", () => {
+  it("renders input box and calculate button", () => {
+    render(<StringCalculator />);
+
+    const inputBox = screen.getByPlaceholderText("Enter numbers...");
+    expect(inputBox).toBeInTheDocument();
+
+    const calculateButton = screen.getByText("Calculate");
+    expect(calculateButton).toBeInTheDocument();
+  });
+
+  it("calculate sum when calculate button is clicked", () => {
+    render(<StringCalculator />);
+    const inputBox = screen.getByTestId("input");
+    const calculateButton = screen.getByTestId("button");
+    fireEvent.change(inputBox, { target: { value: "1,4" } });
+    fireEvent.click(calculateButton);
+    expect(screen.getByText("Output: 5")).toBeInTheDocument();
+  });
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,39 @@
-const StringCalulator = () => {
-  return <h1>String Calculator</h1>;
+"use client";
+import { useState } from "react";
+import { add } from "@/utils/stringCalculator";
+
+const StringCalculator = () => {
+  const [input, setInput] = useState<string>("");
+  const [output, setOutput] = useState<number | string>(0);
+
+  const handleCalculate = () => {
+    try {
+      setOutput(add(input));
+    } catch (error: any) {
+      setOutput(error?.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center p-5 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold mb-5">String Calculator</h1>
+      <input
+        data-testid="input"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="p-2 border rounded w-96"
+        placeholder="Enter numbers..."
+      />
+      <button
+        data-testid="button"
+        onClick={handleCalculate}
+        className="bg-blue-500 px-4 py-2 text-white mt-2 rounded"
+      >
+        Calculate
+      </button>
+      <p className="mt-4 text-xl">{`Output: ${output}`}</p>
+    </div>
+  );
 };
-export default StringCalulator;
+export default StringCalculator;
