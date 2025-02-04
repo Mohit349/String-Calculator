d86d5996f11cad77dfafe44d14a95de0ace26845
Mohit Badgotra <mohitbadgotra@Mohits-MacBook-Pro.local>
2025-02-04 17:45:23 +0530

    feature: handled test cases for single number, two numbers, multiple numbers, new line and negative numbers not allowed

--- utils/stringCalculator.ts
+++ utils/stringCalculator.ts
@@ -1,3 +1,13 @@
-export function add(numbers: string): number | undefined {
+export function add(numbers: string): number | string {
   if (!numbers) return 0;
+  numbers = numbers.replace(/\\n/g, "\n");
+  let delimeter = /,|\n/;
+  const numArray = numbers.split(delimeter).map((one) => Number(one));
+  const negativeNumsArray = numArray.filter((num) => num < 0);
+  if (negativeNumsArray?.length) {
+    throw new Error(
+      `Negative numbers not allowed: ${negativeNumsArray.join(", ")}`
+    );
+  }
+  return numArray.reduce((sum, n) => sum + n, 0);
 }
